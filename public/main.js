d791fbbceae3470ec6fab67cdbdd31cd2686ca20
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-05-19 18:25:40 -0400

    added some comments to the js file to keep trach of tasks. added styling for the shake button

--- public/main.js
+++ public/main.js
@@ -1,7 +1,17 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+// Select the elements on the page - the canvas and the shake buttons
 
-document.addEventListener('DOMContentLoaded', main)
+  // The canvas
+
+  const canvas = document.querySelector('#etch-a-sketch')
+  const ctx = canvas.getContext('2d')
+
+
+// Set our canvas for drawing - see mdn
+
+// write a draw function
+
+// write a handler for the keys
+
+// clear/ shake function
+
+// listen for arrow keys
