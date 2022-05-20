ad65f7ea082ece3b516eef1b622ddf12fafad935
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-05-20 18:38:26 -0400

    creating variables to grab the shake button and the canvas from the dom and create the context for the canvas. added variables for beginning the canvas

--- public/main.js
+++ public/main.js
@@ -3,11 +3,15 @@
   // The canvas
 
   const canvas = document.querySelector('#etch-a-sketch')
+  const shakeBtn = document.querySelector('.shake')
   const ctx = canvas.getContext('2d')
+  console.log(ctx)
 
 
 // Set our canvas for drawing - see mdn
-
+  ctx.lineWidth = round
+  ctx.lineCap = round
+  ctx.lineWidth = 10
 // write a draw function
 
 // write a handler for the keys
