b64f236c71dc0450bc91343b37bc0876195f3abe
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-05-20 18:39:35 -0400

    added a strating point for the canvas following the video. created variables for x and y to have random numbers assigned

--- public/main.js
+++ public/main.js
@@ -9,9 +9,25 @@
 
 
 // Set our canvas for drawing - see mdn
-  ctx.lineWidth = round
-  ctx.lineCap = round
+
+  const width = canvas.width
+  const height = canvas.height
+
+  // Destructuring the above. Make a variable called height and width from
+  // the same properties on our canvas:
+  // const { width, height } = canvas;
+
+  let x = Math.floor(Math.random() * 100)
+  let y = Math.floor(Math.random() * 100)
+
+  ctx.lineJoin = 'round'
+  ctx.lineCap = 'round'
   ctx.lineWidth = 10
+
+  ctx.beginPath()
+  ctx.moveTo(200, 200)
+  ctx.lineTo(200, 200)
+  ctx.stroke()
 // write a draw function
 
 // write a handler for the keys
